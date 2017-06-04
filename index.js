b2de0733b1f8f2d5d2ee4a0585f36998539dd889
Derek Shoemaker <shoemakerdr@gmail.com>
2017-06-04 18:08:32 -0500

    Initialized js file and began work on state, calculator, and controller objects.

---
+++ index.js
@@ -0,0 +1,35 @@
+const state = {
+    value: '0',
+    prev: '',
+    operator: ''
+};
+
+const calc = {
+    add: (a,b) => a + b,
+    subtract: (a,b) => a - b,
+    multiply: (a,b) => a * b,
+    divide: (a,b) => a / b
+};
+
+const controller = {
+    posneg: state => String(Number(state) * -1),
+    percent: state => String(Number(state) / 100),
+    setState: state => {
+        state.value = String(state);
+    },
+    setPrev: () => {
+        state.prev = state.value;
+    },
+    setOperator: op => {
+        state.operator = op;
+    };
+    clear: () => {
+        state.value = '0';
+        state.prev = '';
+    },
+    evaluate: (prev, state, operator) => {
+        const prevNum = Number(prev), stateNum = Number(state);
+        const newState = calc[operator](prevNum, stateNum);
+        controller.setState(newState);
+    }
+};
