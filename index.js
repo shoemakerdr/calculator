be0816e6fe5fff01fbe7fe41504fbe87963a7b08
Derek Shoemaker <shoemakerdr@gmail.com>
2017-06-05 21:05:03 +0000

    Added readyForSecondInput property onto model object to use for clearing state after adding operator. Added number method for setting/adding numbers to state. Completed all numbered and decimal methods. Cleared up evaluate method. TODO: test all methods on controller object.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const model = {
     state: '0',
     prev: '',
     operator: '',
-    hasEvaluated: false
+    hasEvaluated: false,
+    readyForSecondInput: false,
 };
 
 const calc = {
@@ -14,41 +15,95 @@ const calc = {
 };
 
 const controller = {
-    posneg: state => String(Number(state) * -1),
-    percent: state => String(Number(state) / 100),
-    decimal: () => {},
-    one: () => {},
-    two: () => {},
-    three: () => {},
-    four: () => {},
-    five: () => {},
-    six: () => {},
-    seven: () => {},
-    eight: () => {},
-    nine: () => {},
-    zero: () => {},
+    posneg: () => {
+        controller.setState(String(Number(model.state) * -1));
+    },
+    percent: () => {
+        controller.setState(String(Number(model.state) / 100));
+    },
+    decimal: () => {
+        const regex = /\./g;
+        if (!regex.test(model.state)) {
+            controller.setState(`${model.state}.`);
+        }
+    },
+    one: () => {
+        controller.number('1');
+    },
+    two: () => {
+        controller.number('2');
+    },
+    three: () => {
+        controller.number('3');
+    },
+    four: () => {
+        controller.number('4');
+    },
+    five: () => {
+        controller.number('5');
+    },
+    six: () => {
+        controller.number('6');
+    },
+    seven: () => {
+        controller.number('7');
+    },
+    eight: () => {
+        controller.number('8');
+    },
+    nine: () => {
+        controller.number('9');
+    },
+    zero: () => {
+        if (model.state !== '0' || model.operator) {
+            controller.setState(`${model.state}0`);
+        }
+        if (model.readyForSecondInput) {
+            controller.setState('0');
+        }
+    },
+    number: num => {
+        if (model.hasEvaluated || model.state === '0' || model.readyForSecondInput) {
+            controller.setState(num);
+        }
+        else controller.setState(`${model.state}${num}`);
+    },
     setState: state => {
         model.state = state;
+        if (model.hasEvaluated) 
+                model.hasEvaluated = false;
+        if (model.readyForSecondInput) 
+                model.readyForSecondInput = false;
     },
     setPrev: () => {
         model.prev = model.state;
     },
     setOperator: op => {
+        if (model.operator) {
+            controller.evaluate();
+        }
         model.operator = op;
         controller.setPrev();
+        model.readyForSecondInput = true;
     },
     clear: () => {
         model.state = '0';
         model.prev = '';
         model.operator = '';
         model.hasEvaluated = false;
+        model.readyForSecondInput = false;
     },
     evaluate: () => {
-        const prevNum = Number(model.prev), stateNum = Number(model.state);
-        const newState = calc[model.operator](prevNum, stateNum);
-        controller.setState(newState);
-        model.prev = '';
-        model.operator = '';
-        model.hasEvaluated = true;
+        if (model.operator) {
+            const prevNum = Number(model.prev), stateNum = Number(model.state);
+            const newState = calc[model.operator](prevNum, stateNum);
+            controller.setState(newState);
+            model.prev = '';
+            model.operator = '';
+            model.hasEvaluated = true;
+        }
+        else {
+            model.hasEvaluated = true;
+        }
     }
 };
